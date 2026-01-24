a518912e1ff65298727f4feed5b83e663b6dc205
Francesco <francesco-lion@virgilio.it>
2026-01-24 18:43:14 +0100

    chore: removed useless temp cookie and google auth

--- Program.cs
+++ Program.cs
@@ -9,14 +9,6 @@ builder.Services.AddAuthentication(defaultScheme: "Cookie")
       opt.Cookie.Name = "sso-auth";
       opt.ExpireTimeSpan = TimeSpan.FromHours(8);
       opt.LoginPath = "/login";
-    })
-    .AddCookie("TempSessionHandler")
-    .AddGoogle("Google", (opt) =>
-    {
-      opt.ClientId = Environment.GetEnvironmentVariable("SSO_CLIENT_ID") ?? "";
-      opt.ClientSecret = Environment.GetEnvironmentVariable("SSO_CLIENT_SECRET") ?? "";
-      opt.CallbackPath = "/sso-return-url"; // Set this url to google ( defaault: /signin-google )
-      opt.SignInScheme = "TempSessionHandler"; // Store google data into a temporary TempSessionHandler cookie ( default: defaultScheme, "Cookie" )
     });
 
 WebApplication app = builder.Build();
